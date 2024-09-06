0962f852d7589e055b53b288f544459e4057446e
bishal.wow <bishal.devkota@wowfinstacknepal.com>
2024-09-06 17:54:10 +0545

    test: add unit tests for PokemonRepository
    Adds unit tests for the PokemonRepository to verify the save, findAll, and findById functionalities.

--- src/test/java/com/pokemonreview/api/repository/PokemonRepositoryTests.java
+++ src/test/java/com/pokemonreview/api/repository/PokemonRepositoryTests.java
@@ -2,12 +2,16 @@ package com.pokemonreview.api.repository;
 
 import com.pokemonreview.api.models.Pokemon;
 import org.assertj.core.api.Assertions;
+import org.checkerframework.checker.units.qual.A;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+
 @DataJpaTest
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 public class PokemonRepositoryTests {
@@ -30,4 +34,35 @@ public class PokemonRepositoryTests {
         Assertions.assertThat(savedPokemon).isNotNull();
         Assertions.assertThat(savedPokemon.getId()).isGreaterThan(0);
     }
+
+    @Test
+    public void PokemonRepository_GetAll_ReturnMoreThanOnePokemon() {
+        Pokemon pokemon = Pokemon.builder()
+                .name("pikachu")
+                .type("electric").build();
+        Pokemon pokemon2 = Pokemon.builder()
+                .name("pikachu")
+                .type("electric").build();
+        pokemonRepository.save(pokemon);
+        pokemonRepository.save(pokemon2);
+        List<Pokemon> pokemonList = pokemonRepository.findAll();
+
+        Assertions.assertThat(pokemonList).isNotNull();
+        Assertions.assertThat(pokemonList.size()).isEqualTo(2);
+
+    }
+    @Test
+    public void PokemonRepository_findById_ReturnPokemon() {
+        Pokemon pokemon = Pokemon.builder()
+                .name("pikachu")
+                .type("electric").build();
+
+        pokemonRepository.save(pokemon);
+
+        Pokemon pokemonList = pokemonRepository.findById(pokemon.getId()).get();
+
+        Assertions.assertThat(pokemonList).isNotNull();
+
+    }
+
 }
