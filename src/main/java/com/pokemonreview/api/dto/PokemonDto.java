87f3efd49100e4da448f0176273d3e172e2841b1
bishal.wow <bishal.devkota@wowfinstacknepal.com>
2024-09-09 12:49:53 +0545

    feat(model): add Lombok annotations to PokemonDto
    Adds Lombok annotations (`@Data`, `@Builder`, `@NoArgsConstructor`, `@AllArgsConstructor`) to the `PokemonDto` class for convenience. This simplifies the creation and manipulation of PokemonDto instances.

--- src/main/java/com/pokemonreview/api/dto/PokemonDto.java
+++ src/main/java/com/pokemonreview/api/dto/PokemonDto.java
@@ -1,8 +1,15 @@
 package com.pokemonreview.api.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PokemonDto {
     private int id;
     private String name;
