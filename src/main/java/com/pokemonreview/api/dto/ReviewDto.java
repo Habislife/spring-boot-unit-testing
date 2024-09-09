5b4eb3a333f5a824ca2a450fc14bf5c04b497a21
bishal.wow <bishal.devkota@wowfinstacknepal.com>
2024-09-09 13:10:23 +0545

    feat(model): add Lombok annotations to ReviewDto
    Adds Lombok annotations (`@Builder`, `@AllArgsConstructor`, `@NoArgsConstructor`, `@Data`) to the `ReviewDto` class for convenience. This simplifies the creation and manipulation of ReviewDto instances.

--- src/main/java/com/pokemonreview/api/dto/ReviewDto.java
+++ src/main/java/com/pokemonreview/api/dto/ReviewDto.java
@@ -1,7 +1,13 @@
 package com.pokemonreview.api.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class ReviewDto {
     private int id;
